712763b7630ebc7a76c9dc6835a1540a19a1f11e
neoflex-repo <Ne0f1ex>
2018-06-05 12:49:10 +0400

    Detach process of updating dictionaries in the separate microservice 'dict-report'

---
+++ dict-report/dict-report-impl/src/main/java/ru/neoflex/microservices/carpark/dict/report/repository/ReferenceRepository.java
@@ -0,0 +1,12 @@
+package ru.neoflex.microservices.carpark.dict.report.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.neoflex.microservices.carpark.dicts.model.Reference;
+
+/**
+ * @author vanosov
+ */
+@Repository
+public interface ReferenceRepository extends JpaRepository<Reference, Long> {
+}
