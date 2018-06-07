16a6db3391d25b5406c6611e5f5fbf5eba1fbfdb
Aleksandr Nechaev <anechaev@neoflex.ru>
2018-06-07 17:14:01 +0400

    preorder unitest fix

--- preorders/preorders-impl/src/test/java/ru/neoflex/microservices/carpark/preorders/service/KafkaProducerServiceImplTest.java
+++ preorders/preorders-impl/src/test/java/ru/neoflex/microservices/carpark/preorders/service/KafkaProducerServiceImplTest.java
@@ -1,46 +1,56 @@
 package ru.neoflex.microservices.carpark.preorders.service;
 
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.*;
-import ru.neoflex.microservices.carpark.cars.model.NextStatusEvent;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-import static org.mockito.Mockito.*;
-import static org.testng.Assert.*;
-import java.util.Date;
-import java.util.List;
-import java.util.ArrayList;
+import ru.neoflex.microservices.carpark.cars.model.NextStatusEvent;
 
 public class KafkaProducerServiceImplTest {
 
     private String kafkaTopic;
-    @Autowired
-    private KafkaTemplate<String, NextStatusEvent> kafkaTemplate = mock(KafkaTemplate.class);
-    @Autowired
-    private KafkaProducerService kafkaProducerService;
+    private MockMvc mockMvc;
+    @Mock
+    private KafkaTemplate<String, NextStatusEvent> kafkaTemplate;
+    @Mock
+    private ListenableFuture<SendResult<String, NextStatusEvent>> listenableFuture;
+    @InjectMocks
+    private KafkaProducerServiceImpl kafkaProducerService;
 
     private NextStatusEvent nextStatusEvent;
-    private ListenableFuture<SendResult<String, NextStatusEvent>> listenableFuture = mock(ListenableFuture.class);
 
     @BeforeMethod
     public void setupMock() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(kafkaProducerService)
+                .build();
         kafkaTopic = "next.status";
         nextStatusEvent = new NextStatusEvent();
         reset(kafkaTemplate);
         reset(listenableFuture);
-        kafkaProducerService = new KafkaProducerServiceImpl();
     }
 
     @Test
     public void testSendMessage() {
         when(kafkaTemplate.send(null, nextStatusEvent)).thenReturn(listenableFuture);
         when(kafkaTemplate.send(kafkaTopic, nextStatusEvent)).thenReturn(listenableFuture);
-        kafkaTemplate.send(kafkaTopic, nextStatusEvent);
-        kafkaTemplate.send(null, nextStatusEvent);
-        verify(kafkaTemplate, atLeastOnce()).send(eq(kafkaTopic), eq(nextStatusEvent));
+        kafkaProducerService.sendMessage(nextStatusEvent);
+        verify(kafkaTemplate, atLeastOnce()).send(eq(null), eq(nextStatusEvent));
     }
 
 }
