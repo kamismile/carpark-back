5cd16f3d81f6e0ec6b4fd056f67ed5ac6d44fdb0
dbegun <dbegun@neoflex.ru>
2018-06-07 18:09:09 +0300

    sending emails

--- preorders/preorders-impl/src/main/java/ru/neoflex/microservices/carpark/preorders/listener/KafkaConsumer.java
+++ preorders/preorders-impl/src/main/java/ru/neoflex/microservices/carpark/preorders/listener/KafkaConsumer.java
@@ -6,12 +6,13 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
-import ru.neoflex.microservices.carpark.cars.model.Car;
-import ru.neoflex.microservices.carpark.cars.model.NextStatus;
-import ru.neoflex.microservices.carpark.cars.model.NextStatusEvent;
-import ru.neoflex.microservices.carpark.cars.model.PreorderType;
+import ru.neoflex.microservices.carpark.cars.model.*;
 import ru.neoflex.microservices.carpark.cars.service.CarService;
+import ru.neoflex.microservices.carpark.preorders.model.Preorder;
+import ru.neoflex.microservices.carpark.preorders.service.PreorderService;
 
 
 @Slf4j
@@ -19,10 +20,33 @@ import ru.neoflex.microservices.carpark.cars.service.CarService;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class KafkaConsumer {
 
-    @KafkaListener(id = "preorders", topics = "${kafka.orders.topic}",
+    private final PreorderService preorderService;
+
+    private final JavaMailSender emailSender;
+
+    @KafkaListener(id = "preorders", topics = "${kafka.cars.topic}",
             containerFactory = "kafkaListenerContainerFactory")
-    public void listen(ConsumerRecord<String, NextStatusEvent> cr, Acknowledgment acknowledgment) {
+    public void listen(ConsumerRecord<String, CarCommand> cr, Acknowledgment acknowledgment) {
         log.info("received command='{}'", cr.value());
+
+        Car car = cr.value().getEntity();
+        if (States.READY == car.getState()) {
+            Preorder preorder = preorderService.getEarliestPreorderByType(car.getId(), PreorderType.BOOKING);
+            if (preorder != null) {
+                if (car.getCurrentLocationId() != null
+                        && car.getCurrentLocationId().equals(preorder.getStartLocationId())) {
+                    log.info("Sending email to: " + preorder.getEmail());
+                    SimpleMailMessage message = new SimpleMailMessage();
+                    message.setTo(preorder.getEmail());
+                    message.setSubject("Автомобиль ожидает Вас в пункте проката");
+                    message.setText
+                            (String.format("%s %s, здравствуйте! Выбранный Вами автомобиль ожидает в пункте проката.",
+                                    preorder.getClientName(), preorder.getClientPatronymic()));
+                    emailSender.send(message);
+                }
+            }
+        }
+
         acknowledgment.acknowledge();
     }
 }
