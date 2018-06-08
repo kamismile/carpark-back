40d07aa383ff844292e87d0fed8c52902abcd68c
rmorenko <rmorenko@neoflex.ru>
2018-06-08 15:07:57 +0400

    fix code bugs

--- auth/auth-impl/src/main/java/ru/vtb/microservices/carpark/auth/service/UserInfoServiceImpl.java
+++ auth/auth-impl/src/main/java/ru/vtb/microservices/carpark/auth/service/UserInfoServiceImpl.java
@@ -33,8 +33,8 @@ public class UserInfoServiceImpl implements UserInfoService {
     /**
      * Autentification method.
      *
-     * @param login
-     * @param password
+     * @param login login
+     * @param password password
      * @return
     */
     @Override
