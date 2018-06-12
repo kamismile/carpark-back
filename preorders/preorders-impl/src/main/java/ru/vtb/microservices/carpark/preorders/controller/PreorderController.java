cf5cc87eb4342adbc2560b275f03134dd7c5494c
dbegun <dbegun@neoflex.ru>
2018-06-12 18:01:19 +0300

    sonar issue fix

--- preorders/preorders-impl/src/main/java/ru/vtb/microservices/carpark/preorders/controller/PreorderController.java
+++ preorders/preorders-impl/src/main/java/ru/vtb/microservices/carpark/preorders/controller/PreorderController.java
@@ -10,7 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.vtb.microservices.carpark.commons.dto.UserInfo;
 import ru.vtb.microservices.carpark.preorders.api.PreorderApi;
 import ru.vtb.microservices.carpark.preorders.model.Preorder;
@@ -46,8 +50,9 @@ public class PreorderController implements PreorderApi {
         preorder.setCreatedByUser(userInfo.getName());
         return preorderService.addPreorder(preorder, userInfo);
     }
+
     @GetMapping(value = "send", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String sendMail(@RequestParam("to")String mail){
+    public String sendMail(@RequestParam("to") String mail) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(mail);
         message.setSubject("Автомобиль ожидает Вас в пункте проката");
