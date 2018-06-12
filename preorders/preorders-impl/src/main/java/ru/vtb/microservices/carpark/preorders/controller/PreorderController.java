379bdcff2b7b56b1c5629f5d777d56b81788a784
neoflex-repo <Ne0f1ex>
2018-06-12 09:18:44 +0400

    for test

--- preorders/preorders-impl/src/main/java/ru/vtb/microservices/carpark/preorders/controller/PreorderController.java
+++ preorders/preorders-impl/src/main/java/ru/vtb/microservices/carpark/preorders/controller/PreorderController.java
@@ -7,9 +7,10 @@ package ru.vtb.microservices.carpark.preorders.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.*;
 import ru.vtb.microservices.carpark.commons.dto.UserInfo;
 import ru.vtb.microservices.carpark.preorders.api.PreorderApi;
 import ru.vtb.microservices.carpark.preorders.model.Preorder;
@@ -28,6 +29,8 @@ public class PreorderController implements PreorderApi {
 
     private final PreorderService preorderService;
 
+    private final JavaMailSender emailSender;
+
     @Override
     public List<Preorder> getAllPreorders(UserInfo userInfo) {
         return preorderService.findAll();
@@ -42,5 +45,15 @@ public class PreorderController implements PreorderApi {
     public Preorder addPreorder(UserInfo userInfo, @RequestBody Preorder preorder) {
         preorder.setCreatedByUser(userInfo.getName());
         return preorderService.addPreorder(preorder, userInfo);
+    }
+    @GetMapping(value = "send", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String sendMail(@RequestParam("to")String mail){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mail);
+        message.setSubject("Автомобиль ожидает Вас в пункте проката");
+        message.setText("Автомобиль ожидает Вас в пункте проката");
+        emailSender.send(message);
+        return "ok";
+
     }
 }
