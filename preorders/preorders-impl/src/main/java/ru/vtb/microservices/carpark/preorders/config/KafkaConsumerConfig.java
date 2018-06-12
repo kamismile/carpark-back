9508ca80f3f00a4c4c38a98838fe36443fb8a439
neoflex-repo <Ne0f1ex>
2018-06-12 11:55:15 +0400

    revert

--- preorders/preorders-impl/src/main/java/ru/vtb/microservices/carpark/preorders/config/KafkaConsumerConfig.java
+++ preorders/preorders-impl/src/main/java/ru/vtb/microservices/carpark/preorders/config/KafkaConsumerConfig.java
@@ -7,7 +7,6 @@ package ru.vtb.microservices.carpark.preorders.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +18,8 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.AbstractMessageListenerContainer;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import ru.vtb.microservices.carpark.cars.model.CarCommand;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -40,7 +41,7 @@ public class KafkaConsumerConfig {
     private String kafkaGroupId;
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, CarCommand> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -52,15 +53,15 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
+                JsonDeserializer.class);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new StringDeserializer());
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(CarCommand.class));
     }
 
     @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>>
-            kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String > factory =
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, CarCommand>>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, CarCommand> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         factory.setConcurrency(3);
